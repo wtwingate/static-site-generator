e19e88f0c79fd1f353bdf02886f5bab029734d7b
William Wingate <wtwingate@gmail.com>
2024-03-07 15:46:52 -0500

    Write methods and tests for HTMLNode class

---
+++ src/test_htmlnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html1(self):
+        node = HTMLNode("a", "Hello, World", None, {"href": "https://www.boot.dev", "target": "_blank"})
+        result = node.props_to_html()
+        expected = ' href="https://www.boot.dev" target="_blank"'
+        self.assertEqual(result, expected)
+
+
+    def test_props_to_html2(self):
+        node = HTMLNode("h1", "Howdy, Folks!", None, None)
+        result = node.props_to_html()
+        expected = ""
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
