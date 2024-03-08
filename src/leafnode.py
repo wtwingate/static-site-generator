373f39c1690021789f61c7e2f00cb7d1e298c093
William Wingate <wtwingate@gmail.com>
2024-03-08 17:23:26 -0500

    Add message for ValueError

--- src/leafnode.py
+++ src/leafnode.py
@@ -7,7 +7,7 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if self.value is None:
-            raise ValueError
+            raise ValueError("Error: leaf node must have content")
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
