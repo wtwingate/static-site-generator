6a1c1193a117b75198c34f0c1810cdcdc9f67e16
William Wingate <wtwingate@gmail.com>
2024-03-08 23:31:53 -0500

    Write function to convert a text node to an HTML node

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from leafnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
@@ -13,3 +16,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception(f"Invalid text type: {text_node.text_type}")
