3cec76f34356412131bd7fdbb98c0bea342e75dc
William Wingate <wtwingate@gmail.com>
2024-03-08 23:40:07 -0500

    Change function to use if statements

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,14 @@
 from leafnode import LeafNode
 
 
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
@@ -19,18 +27,16 @@ class TextNode:
 
 
 def text_node_to_html(text_node):
-    match text_node.text_type:
-        case "text":
-            return LeafNode(None, text_node.text)
-        case "bold":
-            return LeafNode("b", text_node.text)
-        case "italic":
-            return LeafNode("i", text_node.text)
-        case "code":
-            return LeafNode("code", text_node.text)
-        case "link":
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case "image":
-            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
-        case _:
-            raise Exception(f"Invalid text type: {text_node.text_type}")
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+    raise Exception(f"Invalid text type: {text_node.text_type}")
