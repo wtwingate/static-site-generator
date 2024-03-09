2b4d91952ecfcb8f16a6106b0ce9742f88a6b736
William Wingate <wtwingate@gmail.com>
2024-03-08 19:46:34 -0500

    Create additional tests for the ParentNode class

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -19,6 +19,47 @@ class TestParentNode(unittest.TestCase):
             "<p><b>Bold text</b>Normal text<i>Italic text</i>Normal text</p>",
         )
 
+    def test_parent_with_parents(self):
+        node = ParentNode(
+            "p",
+            [
+                ParentNode(
+                    "div", [LeafNode("p", "hello there"), LeafNode("p", "bye for now")]
+                ),
+                ParentNode(
+                    "span", [LeafNode("p", "hello again"), LeafNode("p", "sayonara")]
+                ),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><div><p>hello there</p><p>bye for now</p></div><span><p>hello again</p><p>sayonara</p></span></p>",
+        )
+
+    def test_deeply_nested_parents(self):
+        node = ParentNode(
+            "div",
+            [
+                ParentNode(
+                    "div",
+                    [
+                        ParentNode(
+                            "div",
+                            [
+                                ParentNode(
+                                    "div", [LeafNode("p", "Wow!", {"class": "wow"})]
+                                )
+                            ],
+                        )
+                    ],
+                )
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<div><div><div><div><p class="wow">Wow!</p></div></div></div></div>',
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
