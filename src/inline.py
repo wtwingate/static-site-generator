c5f7e063a8f5a6577c7eb9334c073164f06c3208
William Wingate <wtwingate@gmail.com>
2024-03-09 21:02:55 -0500

    Rename and rearrange a few things

--- src/inline.py
+++ src/inline.py
@@ -1,3 +1,4 @@
+from textnode import TextNode
 import re
 
 
@@ -11,3 +12,24 @@ def extract_markdown_links(text):
     link_regex = re.compile(r"\[(.*?)\]\((.*?)\)")
     matches = link_regex.findall(text)
     return matches
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
+
+        split_text = node.text.split(delimiter)
+        if len(split_text) % 2 == 0:
+            raise Exception(f"Invalid Markdown syntax: unclosed {delimiter}")
+
+        for index, text in enumerate(split_text):
+            if len(text) == 0:
+                continue
+            if index % 2 == 0:
+                new_nodes.append(TextNode(text, "text"))
+            else:
+                new_nodes.append(TextNode(text, text_type))
+    return new_nodes
--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,6 @@
 from leafnode import LeafNode
 
 
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
-
-
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -26,38 +18,17 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type != text_type_text:
-            new_nodes.append(node)
-            continue
-
-        split_text = node.text.split(delimiter)
-        if len(split_text) % 2 == 0:
-            raise Exception(f"Invalid Markdown syntax: unclosed {delimiter}")
-
-        for index, text in enumerate(split_text):
-            if len(text) == 0:
-                continue
-            if index % 2 == 0:
-                new_nodes.append(TextNode(text, text_type_text))
-            else:
-                new_nodes.append(TextNode(text, text_type))
-    return new_nodes
-
-
 def text_node_to_html(text_node):
-    if text_node.text_type == text_type_text:
+    if text_node.text_type == "text":
         return LeafNode(None, text_node.text)
-    if text_node.text_type == text_type_bold:
+    if text_node.text_type == "bold":
         return LeafNode("b", text_node.text)
-    if text_node.text_type == text_type_italic:
+    if text_node.text_type == "italic":
         return LeafNode("i", text_node.text)
-    if text_node.text_type == text_type_code:
+    if text_node.text_type == "code":
         return LeafNode("code", text_node.text)
-    if text_node.text_type == text_type_link:
+    if text_node.text_type == "link":
         return LeafNode("a", text_node.text, {"href": text_node.url})
-    if text_node.text_type == text_type_image:
+    if text_node.text_type == "image":
         return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
     raise Exception(f"Invalid text type: {text_node.text_type}")
