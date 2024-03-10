3d5d3ceb7a3f1fbcd01fe9ba8d60ee33997bfede
William Wingate <wtwingate@gmail.com>
2024-03-09 23:27:39 -0500

    Remove recursive call from split_nodes_image function

--- src/inline.py
+++ src/inline.py
@@ -38,16 +38,24 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 def split_nodes_image(old_nodes):
     new_nodes = []
     for node in old_nodes:
+        if node.text_type != "text":
+            new_nodes.append(node)
+            continue
         matches = extract_markdown_images(node.text)
         if len(matches) == 0:
             new_nodes.append(node)
             continue
-        split_text = node.text.split(f"![{matches[0][0]}]({matches[0][1]})", 1)
-        if len(split_text[0]) > 0:
-            new_nodes.append(TextNode(split_text[0], "text"))
-        new_nodes.append(TextNode(matches[0][0], "image", matches[0][1]))
-        if len(split_text[1]) > 0:
-            new_nodes.extend(split_nodes_image([TextNode(split_text[1], "text")]))
+        current_text = node.text
+        for match in matches:
+            split_text = current_text.split(f"![{match[0]}]({match[1]})", 1)
+            if len(split_text) != 2:
+                raise Exception("Invalid Markdown syntax")
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], "text"))
+            new_nodes.append(TextNode(match[0], "image", match[1]))
+            current_text = split_text[1]
+        if current_text != "":
+            new_nodes.append(TextNode(current_text, "text"))
     return new_nodes
 
 
