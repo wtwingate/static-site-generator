0222a8d3883995bf0631948f6c9234b8605e61e4
William Wingate <wtwingate@gmail.com>
2024-03-15 15:10:17 -0400

    Write function to generate pages recursively

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from node_markdown import extract_title
 def main():
     reset_public_dir()
     copy_directory_tree("static", "public")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
 
 
 def copy_directory_tree(from_path, dest_path):
@@ -27,6 +27,19 @@ def reset_public_dir():
     if os.path.exists("./public"):
         shutil.rmtree("./public")
     os.mkdir("./public")
+
+
+def generate_pages_recursive(from_path, template_path, dest_path):
+    dir_list = os.listdir(from_path)
+    for item in dir_list:
+        item_path = os.path.join(from_path, item)
+        publish_path = os.path.join(dest_path, item)
+        if os.path.isfile(item_path):
+            if publish_path.endswith(".md"):
+                publish_path = publish_path.replace(".md", ".html")
+            generate_page(item_path, template_path, publish_path)
+        else:
+            generate_pages_recursive(item_path, template_path, publish_path)
 
 
 def generate_page(from_path, template_path, dest_path):
