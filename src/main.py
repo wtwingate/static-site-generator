00d8a09f326a62b241e3d43cb8084307f434a6f3
William Wingate <wtwingate@gmail.com>
2024-03-15 01:19:44 -0400

    Write function to copy static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from node_text import TextNode
+import os
+import re
+import shutil
 
 
 def main():
-    test = TextNode("Quack", "bold", "https://www.mallard.dev")
-    print(test)
+    reset_public_dir()
+    copy_directory_tree("static")
+
+
+def copy_directory_tree(dir_path):
+    dir_list = os.listdir(dir_path)
+    for item in dir_list:
+        item_path = os.path.join(dir_path, item)
+        copy_path = re.sub(r"^static", "public", item_path)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, copy_path)
+        else:
+            os.mkdir(copy_path)
+            copy_directory_tree(item_path)
+
+
+def reset_public_dir():
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    os.mkdir("./public")
 
 
 main()
