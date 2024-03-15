73d0ae8717e218a4e05c1aae22470b61047cfa84
William Wingate <wtwingate@gmail.com>
2024-03-15 01:35:41 -0400

    Rewrite function to avoid using regex

--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
 import os
-import re
 import shutil
 
 
 def main():
     reset_public_dir()
-    copy_directory_tree("static")
+    copy_directory_tree("static", "public")
 
 
-def copy_directory_tree(dir_path):
+def copy_directory_tree(dir_path, dest_path):
     dir_list = os.listdir(dir_path)
+    print(dir_list)
     for item in dir_list:
         item_path = os.path.join(dir_path, item)
-        copy_path = re.sub(r"^static", "public", item_path)
+        copy_path = os.path.join(dest_path, item)
         if os.path.isfile(item_path):
+            print(copy_path)
             shutil.copy(item_path, copy_path)
         else:
             os.mkdir(copy_path)
-            copy_directory_tree(item_path)
+            copy_directory_tree(item_path, copy_path)
 
 
 def reset_public_dir():
