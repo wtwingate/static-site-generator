1c52d6a739156c2483c4e4fc652d258636d67508
William Wingate <wtwingate@gmail.com>
2024-03-23 00:08:17 -0400

    Adds tests for TextNode class

---
+++ tests/test_textnode.py
@@ -0,0 +1,14 @@
+import pytest
+from src.textnode import TextNode
+
+
+class TestTextNode:
+    def test_equal(self):
+        textnode1 = TextNode("Sample text", "bold", "https://www.mallard.dev")
+        textnode2 = TextNode("Sample text", "bold", "https://www.mallard.dev")
+        assert textnode1 == textnode2
+
+    def test_unequal(self):
+        textnode1 = TextNode("Sample text", "bold", "https://www.mallard.dev")
+        textnode2 = TextNode("Different text", "italic", "https://www.example.com")
+        assert textnode1 != textnode2
