813a86115583c4f20fe106463b801acfb2337d68
William Wingate <wtwingate@gmail.com>
2024-03-23 11:52:57 -0400

    Create module to convert text nodes to HTML nodes

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+# Text type constants
+TEXT_TYPE_TEXT = "text"
+TEXT_TYPE_BOLD = "bold"
+TEXT_TYPE_ITALIC = "italic"
+TEXT_TYPE_CODE = "code"
+TEXT_TYPE_LINK = "link"
+TEXT_TYPE_IMAGE = "image"
---
+++ src/convert_node.py
@@ -0,0 +1,19 @@
+from src.leaf_node import LeafNode
+from src.constants import *
+
+
+def text_node_to_leaf_node(text_node):
+    if text_node.text_type == TEXT_TYPE_TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TEXT_TYPE_BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TEXT_TYPE_ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TEXT_TYPE_CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TEXT_TYPE_LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TEXT_TYPE_IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError("Error: invalid text type")
